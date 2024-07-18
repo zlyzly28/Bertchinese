67b648e1b296816c7a565e4c0b6b67ebeaf25fd3
zlyzly <zlyzly@163.com>
2024-07-18 12:40:33 +0800

    bert

--- utils.py
+++ utils.py
@@ -3,7 +3,8 @@ import torch
 from tqdm import tqdm
 import time
 from datetime import timedelta
-
+import torch
+import torch.nn.functional as F
 PAD, CLS = '[PAD]', '[CLS]'  # padding符号, bert中综合信息符号
 
 
@@ -95,3 +96,57 @@ def get_time_dif(start_time):
     end_time = time.time()
     time_dif = end_time - start_time
     return timedelta(seconds=int(round(time_dif)))
+
+class DatasetIterater_b(object):
+    def __init__(self, batches, batch_size, device):
+        self.batch_size = batch_size
+        self.batches = batches
+        self.n_batches = len(batches) // batch_size
+        self.residue = False  # 记录batch数量是否为整数
+        if len(batches) % self.n_batches != 0:
+            self.residue = True
+        self.index = 0
+        self.device = device
+
+    def _to_tensor(self, datas):
+        x = torch.LongTensor([_[0] for _ in datas]).to(self.device)
+        qinggan_y = torch.LongTensor([_[2] for _ in datas]).to(self.device)
+        zhuti_tmp = [_[1] for _ in datas]
+        # print(zhuti_tmp)
+        zhuti_y = torch.stack([torch.sum(F.one_hot(torch.tensor(tmp), 10), axis=0) for tmp in zhuti_tmp]).to(self.device)
+        # y = torch.LongTensor([_[1] for _ in datas]).to(self.device)
+
+        # pad前的长度(超过pad_size的设为pad_size)
+        seq_len = torch.LongTensor([_[3] for _ in datas]).to(self.device)
+        mask = torch.LongTensor([_[4] for _ in datas]).to(self.device)
+        return (x, seq_len, mask), qinggan_y, zhuti_y
+
+    def __next__(self):
+        if self.residue and self.index == self.n_batches:
+            batches = self.batches[self.index * self.batch_size: len(self.batches)]
+            self.index += 1
+            batches = self._to_tensor(batches)
+            return batches
+
+        elif self.index >= self.n_batches:
+            self.index = 0
+            raise StopIteration
+        else:
+            batches = self.batches[self.index * self.batch_size: (self.index + 1) * self.batch_size]
+            self.index += 1
+            batches = self._to_tensor(batches)
+            return batches
+
+    def __iter__(self):
+        return self
+
+    def __len__(self):
+        if self.residue:
+            return self.n_batches + 1
+        else:
+            return self.n_batches
+
+
+def build_iterator_bert(dataset, config):
+    iter = DatasetIterater_b(dataset, config.batch_size, config.device)
+    return iter
